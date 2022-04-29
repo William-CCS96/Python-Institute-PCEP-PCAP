3e0af9c2c70a8da99f5f2c1142338bc85327dbfc
William Correa <wccorreas@gmail.com>
2022-04-29 11:53:36 -0500

    Se cambio el nombre del archivo prueba e inicio de GitHub

---
+++ pruebaInstalacion.py
@@ -0,0 +1,8 @@
+print("Que hubooo...")
+print("Holas de nuevoss")
+print("Ultima prueba y listo..")
+
+print("Aquí de nuevo")
+print("Esta es prueba de lo aprendido corrigiendo errores")
+print("Ultima pruebita")
+
