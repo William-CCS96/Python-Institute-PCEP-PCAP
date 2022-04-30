be9b9503f8ac2bdc6927a3bfd53b22871b78fcdd
William Correa <wccorreas@gmail.com>
2022-04-29 21:16:12 -0500

    Se agrego el archivo literales.py

---
+++ FP1-M2/literales.py
@@ -0,0 +1,57 @@
+print("2")
+print(2)
+
+#Octal
+print(0o123)
+#Hexagecimal
+print(0x123)
+
+#FLOTANTES
+    #2.5
+    #0.4
+    #0.4= .4
+    #4.0= 4.
+#ENTEROS FRENTE A FLOTANTE
+    #4 es un número entero, mientras que 4.0 es un número punto-flotante.
+    #Se puede utilizar la letra e para determinar otros flotante
+        #3E8=300000000=3 x 10-8
+        #Proviene de la palabra exponente
+            #El exponente (el valor después de la E) debe ser un valor entero.
+            #La base (el valor antes de la E) puede o no ser un valor entero.
+#CODIFICANDO FLOTANTE
+    #"La constante de Planck"
+        #6.62607E-34
+        #c=6.62607E-34    
+print(0.0000000000000000000001) 
+#>1e-22
+        #Python siempre elige la presentación más corta del número, y esto se debe de tomar en consideración al crear literales.
+
+#CADENAS
+    #Codificar comillas dentro de una cadena
+print("Me gusta \"Monty Python\"")   # "\" Caracter de Escape
+    #Si se delimita una cadena con una comilla, se debe cerrar con una comilla.
+    #Si se inicia una cadena con un apóstrofe, se debe terminar con un apóstrofe.
+print('Me gusta "Monty Python"')
+#>Me gusta "Monty Python"
+
+    #Imprimir un apostrofe dentro de dos apostrofes
+print('I\'m Monty Python')
+#>I'm Monty Python
+print("I\'m Monty Phyton")
+#>I'm Monty Python
+
+#VALORES BOOLEANOS
+    #Su nombre proviene de: George Boole fue un matemático considerado uno de los padres de las ciencias computacionales en gran medida por su invención del álgebra booleana
+
+    #None
+    #NonTypte: representa laausencia de un valor
+
+#LABORATORIO: Literales de Python - Cadenas
+    #Salida Esperada
+    # "Estoy"
+    # ""aprendiendo""
+    # """Python"""
+print('\"Estoy\"',"\n\"\"aprendiendo\"\"","\n\"\"\"Python\"\"\"")
+
+
+
