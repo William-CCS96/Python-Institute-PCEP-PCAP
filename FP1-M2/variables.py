353458a76a38f1593420857e045447d583bf08fb
William Correa <wccorreas@gmail.com>
2022-04-30 22:34:46 -0500

    Se termino la lección de comentarios

--- FP1-M2/variables.py
+++ FP1-M2/variables.py
@@ -146,6 +146,19 @@ print("y = ",y)
     # y =  -9.0
 
 
+#COMENTARIOS
+
+# este programa calcula los segundos en cierto número de horas determinadas 
+# este programa fue escrito hace dos días
+
+hour = 2 
+seconds = 3600 # número de segundos en una hora
+
+print("Horas: ", hour) 
+print("Segundos en", hour, "horas: ", hour * seconds) # se imprime el numero de segundos en determinado numero de horas
+print("Bay, bay")
+#aquí también se debe de imprimir un "Adiós", pero el programador no tuvo tiempo de escribirlo
+#este el es fin del programa que calcula el numero de segundos en 2 horas
 
 
 
