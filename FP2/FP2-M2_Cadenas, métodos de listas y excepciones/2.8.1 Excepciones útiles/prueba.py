43db577ca82dddafa95f4891c33289b6135e3cf9
William Correa <wccorreas@gmail.com>
2022-06-16 11:21:44 -0500

    Se termino el módulo 2 del Curso No.2 y se inicia el Curso No.3

---
+++ FP2/FP2-M2_Cadenas, métodos de listas y excepciones/2.8.1 Excepciones útiles/prueba.py
@@ -0,0 +1,1 @@
+print("mike">"Mike")
---
+++ FP2/FP2-M2_Cadenas, métodos de listas y excepciones/2.8.1 Excepciones útiles/resumen_Sección.py
@@ -0,0 +1,31 @@
+"""Puntos Clave"""
+
+# 1. Algunas excepciones integradas abstractas de Python son:
+
+        # ArithmeticError.
+        # BaseException.
+        # LookupError.
+
+# 2. Algunas excepciones integradas concretas de Python son:
+
+        # AssertionError.
+        # ImportError.
+        # IndexError.
+        # KeyboardInterrupt.
+        # KeyError.
+        # MemoryError.
+        # OverflowError.
+
+
+"""Ejercicio 1"""
+# ¿Cuál de las excepciones se utilizará para proteger al código de ser interrumpido por el uso del teclado?
+        # KeyboardInterrupt
+
+"""Ejercicio 2"""
+# ¿Cuál es el nombre de la más general de todas las excepciones de Python?
+        # BaseException
+
+"""Ejercicio 3"""
+# ¿Cuál de las excepciones será generada a través de la siguiente evaluación fallida?
+# huge_value = 1E250 ** 2
+        # OverflowError
